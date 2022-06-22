a540a93a17ace4ec5e20b54889543a06f6592603
박지은 <jieun.park@woowahan.com>
2022-06-23 03:35:47 +0900

    medium 문제풀이 추가

---
+++ DeepReadonly.ts
@@ -0,0 +1,21 @@
+type DeepReadonly<T> = keyof T extends never ? T : {
+  readonly [K in keyof T]: DeepReadonly<T[K]>
+};
+
+type X = { 
+  x: { 
+    a: 1
+    b: 'hi'
+  }
+  y: 'hey'
+}
+
+type Expected = { 
+  readonly x: { 
+    readonly a: 1
+    readonly b: 'hi'
+  }
+  readonly y: 'hey' 
+}
+
+type Todo = DeepReadonly<X> // should be same as `Expected`
---
+++ TupleToUnion.ts
@@ -0,0 +1,5 @@
+type TupleToUnion<T> = T extends Array<infer R> ? R : never;
+
+type Arr = ['1', '2', '3']
+
+type Test = TupleToUnion<Arr> // expected to be '1' | '2' | '3'
